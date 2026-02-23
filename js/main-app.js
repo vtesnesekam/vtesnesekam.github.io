7494b718f31b30a77395f4d19d13c34212cfa0b9
vtesnesekam <makesense.iptv@gmail.com>
2026-02-24 07:29:17 +0800

    Update main-app.js

--- js/main-app.js
+++ js/main-app.js
@@ -5,6 +5,21 @@ import { initUI } from './ui-logic.js';
 
 window.addEventListener('DOMContentLoaded', async () => {
   try {
+    const params = new URLSearchParams(window.location.search);
+    const source = params.get("source");
+  
+    if (!source || source === "false") {
+      window.location.href = "pagenotfound.html";
+      return
+    }
+  } catch (e) {
+    window.location.href = "pagenotfound.html";
+    return;
+  }
+
+
+  
+  try {
     await initPlayer();
   } catch (e) {
     console.error('Player init failed', e);
