b6b156a86fcfca8accb767e63b45714fedaa5667
vtesnesekam <makesense.iptv@gmail.com>
2026-02-23 13:05:02 +0800

    Update assets.js

--- js/assets.js
+++ js/assets.js
@@ -1,7 +1,7 @@
 // Simple asset helpers
 
 export async function loadChannels() {
-  const res = await fetch('/js/channels.json', { cache: 'no-store' });
+  const res = await fetch('https://repolevedesnesekam.github.io/development/channels.json', { cache: 'no-store' });
   if (!res.ok) throw new Error('Failed to load channels.json');
   const list = await res.json();
   return list.filter(Boolean);
